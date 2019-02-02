351db33cc31d65ec74600694f6129b4c3edde3ae
Sergey <xxsmotur@gmail.com>
2019-02-02 23:32:27 +0500

    Add iters attributes

--- sampling.py
+++ sampling.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-def reservoir_sampling(data, size):
+def reservoir_sampling(data, size, iters=1):
     result = []
     n = 0
-    for item in data:
-        n += 1
-        if len(result) < size:
-            result.append(item)
-        else:
-            r = np.random.randint(0, n)
-            result[r] = item
+    for it in range(iters):
+        tmp = []
+        for item in data:
+            n += 1
+            if len(tmp) < size:
+                tmp.append(item)
+            else:
+                r = np.random.randint(0, n)
+                if r < size: tmp[r] = item
+        result.append(tmp)
+    return result
