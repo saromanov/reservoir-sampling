2ac98b27405b22bcb3f106121f40552b1a653856
Sergey <xxsmotur@gmail.com>
2019-02-12 18:29:48 +0500

    updating package structure

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
             TODO
            """),
         packages=[
-            "jams",
+            "reservoir",
         ],
         classifiers=[
             "Development Status :: 5 - Production/Stable",
