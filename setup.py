9bb9edf529697654166dea459b7d6ffab6972ae2
Sergey <xxsmotur@gmail.com>
2019-02-12 18:36:27 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@
 
 import setuptools
 import textwrap
-from pip.req import parse_requirements
+try:
+    from pip._internal.req import parse_requirements
+    install_reqs = parse_requirements('requirements.txt', session=False)
+except ImportError:
+    from pip.req import parse_requirements
+    install_reqs = parse_requirements('requirements.txt')
 
 version = "1.0"
 
